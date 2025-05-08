8a2b5a299565422fa77465088e00eef77a627e5f
Amir-Yu <amir.yurista@gmail.com>
2025-05-09 02:19:51 +0300

    Update __init__.py

--- esphome/custom_components/xl4432_spi_sensor/__init__.py
+++ esphome/custom_components/xl4432_spi_sensor/__init__.py
@@ -1,20 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, spi
-from esphome.const import CONF_ID, CONF_METER_ID
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ['spi']
+CODEOWNERS = ['@your_github_username']
 
 xl4432_spi_sensor_ns = cg.esphome_ns.namespace('xl4432_spi_sensor')
 Xl4432SPISensor = xl4432_spi_sensor_ns.class_('Xl4432SPISensor', sensor.Sensor, spi.SPIDevice)
 
 CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Xl4432SPISensor),
-}).extend(spi.spi_device_schema())
+    cv.Optional('meter_id'): cv.ensure_list(cv.hex_uint8_t, cv.Length(min=3, max=3)),
+}).extend(spi.spi_device_schema(cs_pin_required=True))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield spi.register_spi_device(var, config)
     yield sensor.register_sensor(var, config)
     
-    if CONF_METER_ID in config:
-        meter_id = config[CONF_METER_ID]
+    if 'meter_id' in config:
+        meter_id = config['meter_id']
         cg.add(var.set_meter_id(meter_id))
